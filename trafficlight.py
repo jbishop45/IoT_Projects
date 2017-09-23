1a97952bedf151059763e6cf89271ae4d2bfd254
Josh Bishop <jbishop45@gatech.edu>
2017-09-22 21:58:18 -0400

    cleaned up setup loop

--- trafficlight.py
+++ trafficlight.py
@@ -2,27 +2,21 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
+
 SleepTimeL = 0.5
 pinList = [17, 27, 22]
 
-
-GPIO.setmode(GPIO.BCM)
-
 # Setup
-for pin in pinList: 
-    GPIO.setup(pin, GPIO.OUT) 
+for pin in pinList:
+    GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
-    time.sleep(SleepTimeL);
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(SleepTimeL);
-    GPIO.output(pin, GPIO.HIGH)
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(SleepTimeL);
+    time.sleep(SleepTimeL)
 def on(pin):
   GPIO.output(pin, GPIO.LOW)
   print(pin)
-  time.sleep(SleepTimeL);
-  GPIO.output(pin, GPIO.HIGH)  
+  time.sleep(SleepTimeL)
+  GPIO.output(pin, GPIO.HIGH)
 
 # Main loop
 try:
