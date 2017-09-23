f69846763c03f29dc019be98d74bfe2d1b2c09c1
Raymond Moser <rzmoser@gmail.com>
2017-09-23 01:23:29 -0400

    betterTL.py

--- betterTL.py
+++ betterTL.py
@@ -1,24 +1,28 @@
-import RPi.GPIO as GPIO            # import RPi.GPIO module  
+import RPi.GPIO as GPIO            # import RPi.GPIO module 
+import sys 
 from time import sleep             # lets us have a delay  
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
+wait = input('fuck you, enter a #')
+
+
 try:
     while True:
 		GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
-		sleep(2)                   # wait a second
+		sleep(wait)                   # wait a second
 		GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
-		sleep(0.5)                 # wait a second
+		sleep(0.1)                 # wait a second
 		GPIO.output(27, 1)          
-		sleep(2)
+		sleep(wait)
 		GPIO.output(27, 0)
-		sleep(0.5)
+		sleep(wait)
 		GPIO.output(22, 1)
-		sleep(2)
+		sleep(wait)
 		GPIO.output(22, 0)
-		sleep(0.5)
+		sleep(wait)
 
 
 
